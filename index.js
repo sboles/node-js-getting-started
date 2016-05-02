8e09684445799fc8943b50982bb5326d09d59bd3
Steven Boles <boles.steven@gmail.com>
2016-05-02 19:32:44 -0400

    added times endpoint

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
+
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
